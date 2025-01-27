03b3d0798a59628b8e87aa79038da95b5a2601dc
Alessia Fella <alessiafell4@gmail.com>
2025-01-27 09:04:14 +0100

    Update imposte.py

--- imposte.py
+++ imposte.py
@@ -1,23 +1,23 @@
 reddito =int(input ("inserisci il tuo reddito: "))
 
-seconda_aliquota= 0.23
-terza_aliquota= 0.35
-quarta_aliquota= 0.43
+prima_aliquota= 0.23
+seconda_aliquota= 0.35
+terza_aliquota= 0.43
 
 
 if reddito <= 28000 :
-  aliquote_due = seconda_aliquota*(reddito)
+  aliquote_due = prima_aliquota*(reddito)
   totale_da_pagare = reddito-(aliquote_due)
 
 elif 28001 <= reddito <= 50000 :
-  aliquote_due = seconda_aliquota*(28000)
-  aliquote_tre= terza_aliquota*(reddito-28001) 
+  aliquote_due = prima_aliquota*(28000)
+  aliquote_tre= seconda_aliquota*(reddito-28001) 
   totale_da_pagare = reddito - ( aliquote_due + aliquote_tre)
 
 else  :
-  aliquote_due = seconda_aliquota*(28000)
-  aliquote_tre= terza_aliquota*(21999) 
-  aliquote_quattro= quarta_aliquota*(reddito-50000)
+  aliquote_due = prima_aliquota*(28000)
+  aliquote_tre= seconda_aliquota*(21999) 
+  aliquote_quattro= terza_aliquota*(reddito-50000)
   totale_da_pagare = reddito - ( aliquote_due + (aliquote_tre)+ aliquote_quattro)
   
 
